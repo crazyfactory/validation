3ceface834da3b4945a662da5f20e931ee74f6ee
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-10-10 13:32:44 +0700

    test: fix imports

--- tests/unit/ZipCodeTest.php
+++ tests/unit/ZipCodeTest.php
@@ -2,30 +2,33 @@
 
 namespace CrazyFactory\Validation\Tests;
 
+use CrazyFactory\Validation\ZipCode\Sanitizer;
+use CrazyFactory\Validation\ZipCode\Validator;
+
 class ZipCodeTest extends \Codeception\Test\Unit
 {
     public function testIsValid()
     {
-        $this->assertFalse(ZipCodeValidator::isValid('', 'AT'), 'empty');
-        $this->assertFalse(ZipCodeValidator::isValid('A', 'DE'), 'DE: A');
-        $this->assertFalse(ZipCodeValidator::isValid('01234', 'LU'), 'LU');
-        $this->assertFalse(ZipCodeValidator::isValid('01234', 'RO'), 'RO');
-        $this->assertTrue(ZipCodeValidator::isValid('123456', 'RO'), 'RO');
-        $this->assertTrue(ZipCodeValidator::isValid('01234', 'FR'), 'FR');
-        $this->assertTrue(ZipCodeValidator::isValid('M6 6SD', 'GB'), 'GB: M6 6SD');
+        $this->assertFalse(Validator::isValid('', 'AT'), 'empty');
+        $this->assertFalse(Validator::isValid('A', 'DE'), 'DE: A');
+        $this->assertFalse(Validator::isValid('01234', 'LU'), 'LU');
+        $this->assertFalse(Validator::isValid('01234', 'RO'), 'RO');
+        $this->assertTrue(Validator::isValid('123456', 'RO'), 'RO');
+        $this->assertTrue(Validator::isValid('01234', 'FR'), 'FR');
+        $this->assertTrue(Validator::isValid('M6 6SD', 'GB'), 'GB: M6 6SD');
     }
 
     public function testSanitize()
     {
-        $this->assertSame('', ZipCodeSanitizer::sanitize('', 'GB'), 'empty');
-        $this->assertSame('M6 6SD', ZipCodeSanitizer::sanitize('M6 6 sd', 'GB'), 'GB');
-        $this->assertSame('M61 6SD', ZipCodeSanitizer::sanitize('M6 16 sD', 'GB'), 'GB');
-        $this->assertSame('M621 6SD', ZipCodeSanitizer::sanitize('M62 16 Sd', 'GB'), 'GB');
-        $this->assertSame('12345', ZipCodeSanitizer::sanitize('1 23 45', 'FR'), 'FR');
-        $this->assertSame('12345', ZipCodeSanitizer::sanitize('1 23 45', 'FR'), 'FR');
-        $this->assertSame('1234 XY', ZipCodeSanitizer::sanitize('1234XY', 'NL'), 'NL');
-        $this->assertSame('123 45', ZipCodeSanitizer::sanitize('12345', 'CZ'), 'CZ');
-        $this->assertSame('123 45', ZipCodeSanitizer::sanitize('1 234 5', 'CZ'), 'CZ');
-        $this->assertSame('CantFix', ZipCodeSanitizer::sanitize('CantFix', 'FI'), 'cant fix');
+        $this->assertSame('', Sanitizer::sanitize('', 'GB'), 'empty');
+        $this->assertSame('M6 6SD', Sanitizer::sanitize('M6 6 sd', 'GB'), 'GB');
+        $this->assertSame('M61 6SD', Sanitizer::sanitize('M6 16 sD', 'GB'), 'GB');
+        $this->assertSame('M621 6SD', Sanitizer::sanitize('M62 16 Sd', 'GB'), 'GB');
+        $this->assertSame('12345', Sanitizer::sanitize('1 23 45', 'FR'), 'FR');
+        $this->assertSame('12345', Sanitizer::sanitize('1 23 45', 'FR'), 'FR');
+        $this->assertSame('1234 XY', Sanitizer::sanitize('1234XY', 'NL'), 'NL');
+        $this->assertSame('123 45', Sanitizer::sanitize('12345', 'CZ'), 'CZ');
+        $this->assertSame('123 45', Sanitizer::sanitize('1 234 5', 'CZ'), 'CZ');
+        $this->assertSame('CantFix', Sanitizer::sanitize('CantFix', 'FI'), 'cant fix');
     }
 }
