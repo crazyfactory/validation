70d99c425c7fa4ce6ea6b3bf6dc28989d71c0e5f
Turbo <turbo@crazy-factory.co.th>
2019-06-13 16:07:52 +0700

    feat: option to bypass remote error

--- src/Eori/EoriValidator.php
+++ src/Eori/EoriValidator.php
@@ -7,23 +7,44 @@ use SoapClient;
 class EoriValidator
 {
     private $soapClient;
+    private $byPassRemoteError = true;
 
     public function __construct(SoapClient $client = null)
     {
         $this->soapClient = $client ?? new SoapClient('https://ec.europa.eu/taxation_customs/dds2/eos/validation/services/validation?wsdl');
     }
 
+    public function setByPassRemoteError(bool $byPassRemoteError): EoriValidator
+    {
+        $this->byPassRemoteError = $byPassRemoteError;
+
+        return $this;
+    }
+
     /**
      * @param string $eori
      * @return bool
+     * @throws \SoapFault
      */
     public function validate(string $eori): bool
     {
-        $response = $this->soapClient->__soapCall('validateEORI', ['validateEORI' => ['eori' => $eori]]);
+        try {
+            $response = $this->soapClient->__soapCall('validateEORI', ['validateEORI' => ['eori' => $eori]]);
 
-        // 0 = success
-        // 1 = failed
+            // 0 = success
+            // 1 = failed
 
-        return $response->result->status === 0;
+            return $response->result->status === 0;
+        } catch (\SoapFault $e) {
+            if ($e->faultcode === 'WSDL') {
+                return false;
+            }
+
+            if ($this->byPassRemoteError) {
+                return true;
+            }
+
+            throw $e;
+        }
     }
 }
--- tests/unit/EoriValidatorTest.php
+++ tests/unit/EoriValidatorTest.php
@@ -7,11 +7,6 @@ use CrazyFactory\Validation\Eori\EoriValidator;
 
 class EoriValidatorTest extends \Codeception\Test\Unit
 {
-    /**
-     * @param $eori
-     * @param $expected
-     * @throws \Exception
-     */
     public function testValidate()
     {
         $response = new \stdClass();
@@ -35,5 +30,55 @@ class EoriValidatorTest extends \Codeception\Test\Unit
         $validator = new EoriValidator($soapClient);
         $result = $validator->validate('1234');
         $this->assertFalse($result);
+    }
+
+    public function testValidateWithWSDLFaultCode()
+    {
+        $response = new \stdClass();
+        $response->result = new \stdClass();
+        // Failed
+        $response->result->status = 1;
+        $soapClient = Stub::makeEmpty(\SoapClient::class, [
+            '__soapCall' => function () {
+                throw new \SoapFault('WSDL', 'wsdl error');
+            }
+        ]);
+        $validator = new EoriValidator($soapClient);
+        $result = $validator->validate('x');
+        $this->assertFalse($result);
+    }
+
+    public function testValidateWithByPassRemoteError()
+    {
+        $response = new \stdClass();
+        $response->result = new \stdClass();
+        // Failed
+        $response->result->status = 1;
+        $soapClient = Stub::makeEmpty(\SoapClient::class, [
+            '__soapCall' => function () {
+                throw new \SoapFault('soap:Server', 'remote server error');
+            }
+        ]);
+        $validator = new EoriValidator($soapClient);
+        $validator->setByPassRemoteError(true);
+        $result = $validator->validate('x');
+        $this->assertTrue($result);
+    }
+
+    public function testValidateWithBNotByPassRemoteError()
+    {
+        $response = new \stdClass();
+        $response->result = new \stdClass();
+        // Failed
+        $response->result->status = 1;
+        $soapClient = Stub::makeEmpty(\SoapClient::class, [
+            '__soapCall' => function () {
+                throw new \SoapFault('soap:Server', 'remote server error');
+            }
+        ]);
+        $validator = new EoriValidator($soapClient);
+        $validator->setByPassRemoteError(false);
+        $this->expectException(\SoapFault::class);
+        $validator->validate('x');
     }
 }
