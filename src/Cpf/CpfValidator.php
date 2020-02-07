acbfa36a13ff2a25cc587e69f09d4f36f92a19ed
paibamboo <gavinsitti@hotmail.com>
2020-02-07 14:29:14 +0700

    style: lint fix

--- src/Cpf/CpfValidator.php
+++ src/Cpf/CpfValidator.php
@@ -4,8 +4,8 @@ namespace CrazyFactory\Validation\Cpf;
 
 class CpfValidator
 {
-	public static function isValid(string $cpf): bool
-	{
-		return preg_match('/^[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]-[0-9][0-9]$/', $cpf) > 0;
-	}
+    public static function isValid(string $cpf): bool
+    {
+        return preg_match('/^[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]-[0-9][0-9]$/', $cpf) > 0;
+    }
 }
--- tests/unit/CpfValidatorTest.php
+++ tests/unit/CpfValidatorTest.php
@@ -6,12 +6,12 @@ use CrazyFactory\Validation\Cpf\CpfValidator;
 
 class CpfValidatorTest extends \Codeception\Test\Unit
 {
-	public function testIsValid()
-	{
-		$this->assertTrue(CpfValidator::isValid('123.456.789-12'));
-		$this->assertFalse(CpfValidator::isValid('0.123.456.789-12'));
-		$this->assertFalse(CpfValidator::isValid('123.456.789-123'));
-		$this->assertFalse(CpfValidator::isValid('abc'));
-		$this->assertFalse(CpfValidator::isValid('123'));
-	}
+    public function testIsValid()
+    {
+        $this->assertTrue(CpfValidator::isValid('123.456.789-12'));
+        $this->assertFalse(CpfValidator::isValid('0.123.456.789-12'));
+        $this->assertFalse(CpfValidator::isValid('123.456.789-123'));
+        $this->assertFalse(CpfValidator::isValid('abc'));
+        $this->assertFalse(CpfValidator::isValid('123'));
+    }
 }
