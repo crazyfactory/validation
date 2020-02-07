9e5bacb4da9fb5906b1c4cccd6794795f28038f9
paibamboo <gavinsitti@hotmail.com>
2020-02-07 14:17:18 +0700

    feat: cpf validation

---
+++ src/Cpf/Validator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CrazyFactory\Validation\Cpf;
+
+class CpfValidator
+{
+	public static function isValid(string $cpf): bool
+	{
+		return preg_match('/^[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]-[0-9][0-9]$/', $cpf) > 0;
+	}
+}
