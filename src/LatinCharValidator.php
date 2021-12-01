bae722cc690f37e4372bc8cd6946a08146044c7c
Turbo <p.petpadriew@gmail.com>
2021-12-01 17:24:33 +0700

    style: format

--- src/LatinCharValidator.php
+++ src/LatinCharValidator.php
@@ -6,12 +6,14 @@ class LatinCharValidator
 {
     /**
      * exclude C0 controls (basic latin): https://unicode-table.com/en/blocks/basic-latin/
-     * @param  string  $txt
+     *
+     * @param string $txt
      *
      * @return bool
      */
     public static function isValid(string $txt): bool
     {
-        return empty($txt) || (preg_match('/^[\p{Latin}\p{Common}]+$/u', $txt) > 0 && preg_match('/[\x{0000}-\x{001F}]/u', $txt) == 0 );
+        return empty($txt) || (preg_match('/^[\p{Latin}\p{Common}]+$/u', $txt) > 0
+                && preg_match('/[\x{0000}-\x{001F}]/u', $txt) == 0);
     }
 }
