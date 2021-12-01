9f592e3b8bb9f077f5ae9010ca52ae67453c8b3b
lalita <lalitachuam@gmail.com>
2021-12-01 10:01:01 +0700

    chore(latinValidator): prevent emoji

--- src/LatinCharValidator.php
+++ src/LatinCharValidator.php
@@ -6,6 +6,6 @@ class LatinCharValidator
 {
     public static function isValid(string $txt): bool
     {
-        return empty($txt) || preg_match('/^[\p{Latin}\p{Common}]+$/u', $txt) > 0;
+        return empty($txt) || (preg_match('/^[\p{Latin}\p{Common}]+$/u', $txt) > 0 && preg_match('/[^\w\x{0080}-\x{FFFF}]+$/u', $txt) == 0 );
     }
 }
